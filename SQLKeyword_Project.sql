9e56269dd7c9bb45c573d5d73ec8e2333c53bb05
chrisshuo123 <chr1z100@gmail.com>
2025-04-08 17:38:01 +0700

    Add table profile and namaTokoMarketplace in Group Profile

--- SQLKeyword_Project.sql
+++ SQLKeyword_Project.sql
@@ -1,1 +1,18 @@
 use keyword_db_mp_experiment;
+
+/* 1 - Group Profile */
+create table profile (
+	idProfile int(10) primary key auto_increment,
+    tanggalInput timestamp not null default current_timestamp,
+    jenisMP varchar(10)
+);
+describe profile;
+
+create table namaTokoMarketplace (
+	idNamaToko int(10) primary key auto_increment,
+    idToko int(10)
+);
+alter table namaTokoMarketplace
+	add column tanggalInput timestamp not null default current_timestamp after idNamaToko;
+describe namaTokoMarketplace;
+
