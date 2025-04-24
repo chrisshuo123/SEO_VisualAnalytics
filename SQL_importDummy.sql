93d111660b38f4c5f0cc1d65f57cf7b326c489bc
chrisshuo123 <chr1z100@gmail.com>
2025-04-24 22:22:51 +0700

    Upload dummy data for 'Ads 3: Laporan 'Iklan Produk Pencarian' Platform A'

--- SQL_importDummy.sql
+++ SQL_importDummy.sql
@@ -1,5 +1,17 @@
 /* Ads 1: Laporan 'Semua Iklan Produk' Platform A (Laporan Dummy) */
 LOAD DATA INFILE 'D:/5 - PixelMind ID (Coding, Photo Editing, Digital Marketing)/33.1_Keyword MP DB/2 - Untuk Portfolio & Project/SQL Keyword Project & Experiment/csvDataDummies_foodAPIMimesis/platform_a/1_hasil_laporan_iklan_platform_a.csv'
+INTO TABLE semua_iklan_produk_platform_a
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(namaGrup,namaProduk,pengeluaran,pendapatan,totalEfektivitasIklan,efektivitasIklan,performaTampil,tampilTeratas,tampil,klik,persentaseKlik,terjual,totTerjual,persentaseTerjual,@rataRata)
+SET
+	rataRata = CAST(@rataRata AS DECIMAL(10,2));
+
+select * from semua_iklan_produk_platform_a;
+
+/* Ads 2: Laporan 'Iklan Produk Direkomendasi' Platform A (Laporan Dummy) */
+LOAD DATA INFILE 'D:/5 - PixelMind ID (Coding, Photo Editing, Digital Marketing)/33.1_Keyword MP DB/2 - Untuk Portfolio & Project/SQL Keyword Project & Experiment/csvDataDummies_foodAPIMimesis/platform_a/2_iklan_produk_direkomendasi_platform_a.csv'
 INTO TABLE iklan_produk_direkomendasi_platform_a
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -9,4 +21,18 @@ SET
 	rataRata = CAST(@rataRata AS DECIMAL(10,2));
 
 select * from iklan_produk_direkomendasi_platform_a;
-/* Berjumlah 89 rows, dengan PK idIklanRekomendasi berkisar 1-89 */
+
+/* Ads 3: Laporan 'Iklan Produk Pencarian' Platform A (Laporan Dummy) */
+LOAD DATA INFILE 'D:/5 - PixelMind ID (Coding, Photo Editing, Digital Marketing)/33.1_Keyword MP DB/2 - Untuk Portfolio & Project/SQL Keyword Project & Experiment/csvDataDummies_foodAPIMimesis/platform_a/3_iklan_produk_pencarian_platform_a.csv'
+INTO TABLE iklan_produk_pencarian_platform_a
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(namaGrup,namaProduk,pengeluaran,pendapatan,totalEfektivitasIklan,efektivitasIklan,performaTampil,tampilTeratas,tampil,klik,persentaseKlik,terjual,totTerjual,persentaseTerjual,@rataRata)
+SET
+	rataRata = CAST(@rataRata AS DECIMAL(10,2));
+
+select * from iklan_produk_pencarian_platform_a;
+
+
