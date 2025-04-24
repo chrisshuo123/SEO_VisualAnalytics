8a1caacd02bf805233a3cd5e53a6143ab943fd91
chrisshuo123 <chr1z100@gmail.com>
2025-04-24 21:51:59 +0700

    Upload dummy data for 'Ads 1: Laporan 'Semua Iklan Produk' Platform A'

--- SQL_importDummy.sql
+++ SQL_importDummy.sql
@@ -1,6 +1,6 @@
-/* Ads 2: Iklan Produk Rekomend Tokped, Laporan 1-31 Jan 2024 Long Tail */
-LOAD DATA INFILE 'D:/5 - PixelMind ID (Coding, Photo Editing, Digital Marketing)/33.1_Keyword MP DB/1 - Untuk Menampung Tokped & Sope/File SQL/csv_file/1_Januari_2024/2_iklan_produk_direkomendasi_1_31_jan_24.csv'
-INTO TABLE iklan_produk_rekomend_tokped
+/* Ads 1: Laporan 'Semua Iklan Produk' Platform A (Laporan Dummy) */
+LOAD DATA INFILE 'D:/5 - PixelMind ID (Coding, Photo Editing, Digital Marketing)/33.1_Keyword MP DB/2 - Untuk Portfolio & Project/SQL Keyword Project & Experiment/csvDataDummies_foodAPIMimesis/platform_a/1_hasil_laporan_iklan_platform_a.csv'
+INTO TABLE iklan_produk_direkomendasi_platform_a
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
